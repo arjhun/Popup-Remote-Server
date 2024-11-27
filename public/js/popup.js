53a99473ad622f5592915ab7f9ae933c8cf82ead
arjhun <arjenklaverstijn@gmail.com>
2024-11-27 12:15:13 +0100

    some refractoring, added optional title field and extracted js to its own file

---
+++ public/js/popup.js
@@ -0,0 +1,47 @@
+const hiddenHeight = 600;
+const popupHidden = { marginBottom: `-${hiddenHeight}px` };
+
+let $popup, $content, $title;
+
+$(function () {
+  $popup = $(".popup");
+  $content = $popup.find("#content");
+  $title = $popup.find("#title");
+});
+
+const socket = io({
+  extraHeaders: {
+    clientType: "endpoint",
+  },
+});
+
+socket.on("popup", (popup) => {
+  if (popup) showPopup(popup);
+});
+
+socket.on("hide", function () {
+  hidePopup();
+});
+
+function setContent(popup) {
+  if (popup?.title)
+    $title.html(`<span class=" glow">${popup.title}</span> asked:`);
+  $content.text(popup.content);
+}
+
+function hidePopup() {
+  $popup.animate(popupHidden, 500, () => {});
+}
+
+function showPopup(popup) {
+  if (parseFloat($popup.css("marginBottom")) >= -hiddenHeight) {
+    $popup.animate(popupHidden, 500, () => {
+      $popup.clearQueue();
+      setContent(popup);
+      $popup.animate({ marginBottom: "50px" }, 1000);
+    });
+  } else {
+    setContent(popup);
+    $popup.animate({ marginBottom: "100px" }, 1000);
+  }
+}
