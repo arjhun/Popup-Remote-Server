eaabe3b625da90bf6625d983c0118addd1d847b3
arjhun <arjenklaverstijn@gmail.com>
2024-11-27 22:21:44 +0100

    added redaction for password in logs

--- logger.js
+++ logger.js
@@ -3,6 +3,7 @@ import { pino } from "pino";
 import config from "./server.config.js";
 const logger = pino({
   level: config.log.logLevel,
+  redact: ["password", "token"],
 });
 
 export default logger;
